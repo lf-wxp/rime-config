617b6cea041d438c333bab3cd31faf0b2dc13ece
franciscowxp <franciscowxp@gmail.com>
2023-10-09 20:15:12 +0800

    feat: add bare config

---
+++ setup.sh
@@ -0,0 +1,5 @@
+brew install --cask squirrel
+curl -fsSL https://raw.githubusercontent.com/rime/plum/master/rime-install | bash
+bash plum/rime-install GuoBinyong/wubixinshiji pinyin-simp
+cp default.custom.yaml ~/Library/Rime
+cp squirrel.custom.yaml ~/Library/Rime
